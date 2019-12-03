832f98e6aedf5b9d12c080a368fad664760f9b2a
dariusmaximus <dariulqa@wp.pl>
2019-12-03 18:33:21 +0100

    checkbox added, task object modified

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -2,11 +2,19 @@ import React from 'react';
 
 function ListItem(props) {
     const tasks = props.tasks;
+
     return tasks.map((element, index) => {
-    console.log(element, index);
-    return <li key={index}>{element.name}: {element.time}<button onClick={() => props.delete(index)}>delete</button></li>
-    }
-    )
+        return <li className="greyscale" key={index}>{element.name}: {element.time}<button onClick={() => props.delete(index)}>delete</button>
+            <label className="container">
+                <input type="checkbox" checked={element.checked} onClick={(e) => {
+                    let checked = e.target.checked;
+                    props.checked(checked, index);
+                    console.log('THIS IS THE INDEX PASSED IN PROPS', index)
+                    }}></input>
+                <span className="checkmark"></span>
+            </label>
+        </li>
+    })
 }
 
 export default ListItem;
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,19 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import ListItem from './ToDoItem';
 
-let initialList = {
-    name: '',
-    time: '',
-    finished: false
-}
-
 const ToDoList = () => {
+    let [counter, setCounter] = useState(0);
+    console.log('THIS IS MY COUNTER', counter);
+    let initialList = {
+        id: counter,
+        name: '',
+        time: '',
+        finished: false,
+    }
+
     const [tasks, setTasks] = useState([]);
-    const [task, setTask] = useState(initialList);
     useEffect(() => console.log('THE ARRAY OF TASKS', tasks), [tasks]);
+
+    const [task, setTask] = useState(initialList);
+    useEffect(() => console.log('THE CURRENT TASK', task), [task]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        setCounter(prevState => {return counter + 1});
+        setTask(prevState => {
+            return { ...prevState, id: counter + 1}
+        })
         setTasks([...tasks, task]);
         setTask(initialList);
     }
@@ -25,26 +34,35 @@ const ToDoList = () => {
         setTasks(newTasks);
     }
 
+    const handleCheckbox = (val, id) => {
+        tasks.map((element, index) => {
+            if (index === id) {
+                element.finished = val
+            }
+        })
+        console.log('UPDATED ARR', tasks)
+    }
+
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <input placeholder="Add a task" onChange={(e) => {
                     let value = e.target.value;
                     setTask(prevState => {
-                        return {...prevState, name: value}
+                        return { ...prevState, name: value }
                     })
-                }} 
-                value={task.name}></input>
+                }}
+                    value={task.name}></input>
 
                 <input placeholder="Add time" onChange={(e) => {
                     let value = e.target.value;
                     setTask(prevState => {
-                        return {...prevState, time: value}
+                        return { ...prevState, time: value }
                     })
                 }} value={task.time}></input>
                 <button>Submit</button>
             </form>
-            <ListItem tasks={tasks} delete={deleteItem}/>
+            <ListItem tasks={tasks} delete={deleteItem} checked={handleCheckbox} />
         </div>
     )
 }
