1a1608dc1f1e2adc5999f2cd76164211cbf1d387
dariusmaximus <dariulqa@wp.pl>
2019-12-04 22:09:17 +0100

    Updates

---
+++ precache-manifest.201af6fed213551585599cf7286c6b82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d656e56509a0e4adbc06e9b26a10c649",
+    "url": "/todolistWithReactHooks/index.html"
+  },
+  {
+    "revision": "28ffb9e4e87fb445b0dc",
+    "url": "/todolistWithReactHooks/static/css/main.c49b7231.chunk.css"
+  },
+  {
+    "revision": "6e032408847c3872411a",
+    "url": "/todolistWithReactHooks/static/js/2.99be9dc0.chunk.js"
+  },
+  {
+    "revision": "28ffb9e4e87fb445b0dc",
+    "url": "/todolistWithReactHooks/static/js/main.ddded60e.chunk.js"
+  },
+  {
+    "revision": "2c029583b38a13685314",
+    "url": "/todolistWithReactHooks/static/js/runtime-main.0b99eecf.js"
+  }
+]);
