5712f86d91727ad64d9978a1e974e6deafd825d3
dariusmaximus <dariulqa@wp.pl>
2019-12-04 15:13:39 +0100

    checkbox bug fixed, material ui installed

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,23 +1,41 @@
 import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
 
+const StyledButton = withStyles({
+    root: {
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      borderRadius: 3,
+      border: 0,
+      color: 'white',
+      height: 48,
+      padding: '0 30px',
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    },
+    label: {
+      textTransform: 'capitalize',
+    },
+  })(Button);
 
 function ListItem(props) {
     const tasks = props.tasks;
     const [isChecked, setIsChecked] = useState(false);
 
     return tasks.map((element, index) => {
-
-        return <li className="greyscale" key={index}>{element.name}: {element.time}
-            <button onClick={() => props.delete(index)}>delete</button>
+        console.log(element.finished)
+        return <li className={element.finished ? 'listitem greyscale' : 'lititem'} key={index}><span>{element.name}: {element.time}</span>
+            <StyledButton onClick={() => props.delete(index)}>delete</StyledButton>
             <label className="container">
                 <input type="checkbox" checked={element.finished}
-                    onChange={(e) => setIsChecked(!isChecked)} onClick={(e) => {
-                        props.checked(isChecked, element.id);
-                    }}></input>
-                <span className="checkmark"></span>
+                    onChange={() => {
+                        setIsChecked(!isChecked);
+                        props.checked(!element.finished, element.id)
+                        }}></input>
+                <span className="checkmark">{isChecked}</span>
             </label>
         </li>
     })
 }
+
 
 export default ListItem;
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,5 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import ListItem from './ToDoItem';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
+const SubmitButton = withStyles({
+    root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      },
+      label: {
+        textTransform: 'capitalize',
+      },
+})(Button);
 
 const ToDoList = () => {
     let [counter, setCounter] = useState(0);
@@ -61,7 +78,7 @@ const ToDoList = () => {
                         return { ...prevState, time: value }
                     })
                 }} value={task.time}></input>
-                <button>Submit</button>
+                <SubmitButton type="submit">Submit</SubmitButton>
             </form>
             <ListItem tasks={tasks} delete={deleteItem} checked={handleCheckbox} />
         </div>
